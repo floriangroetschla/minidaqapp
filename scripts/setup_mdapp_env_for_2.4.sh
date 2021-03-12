7b9715d4880268cebd3453158bcf279115fb3284
Kurt Biery <biery@fnal.gov>
2021-03-12 12:25:09 -0600

    updated dfmodules and minidaqapp versions in setup script

--- scripts/setup_mdapp_env_for_2.4.sh
+++ scripts/setup_mdapp_env_for_2.4.sh
@@ -57,7 +57,7 @@ function clone_repo_for_mdapp {
 # Clone the repos that we want
 clone_repo_for_mdapp dataformats develop v2.0.0
 clone_repo_for_mdapp dfmessages develop v2.0.0
-clone_repo_for_mdapp dfmodules develop v2.0.1
+clone_repo_for_mdapp dfmodules develop v2.0.2
 clone_repo_for_mdapp flxlibs develop
 clone_repo_for_mdapp ipm develop v2.0.1
 clone_repo_for_mdapp nwqueueadapters develop v1.1.1
@@ -65,7 +65,7 @@ clone_repo_for_mdapp readout develop
 clone_repo_for_mdapp restcmd develop
 clone_repo_for_mdapp serialization develop v1.1.0
 clone_repo_for_mdapp trigemu develop v2.0.2
-clone_repo_for_mdapp minidaqapp glm/multi_app
+clone_repo_for_mdapp minidaqapp develop v2.0.0
 
 # Next, update the dbt-build-order.cmake file
 cp -p dbt-build-order.cmake dbt-build-order.cmake.orig
